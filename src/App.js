891b0950cfef705fe7d03224992b6a3b287669c8
Daniel Deverell <daniel.deverell@gmail.com>
2022-11-17 19:39:43 -0600

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Recipes from "./Recipes";
+import RecipeDetail from "./RecipeDetail";
+import { useFetch } from "./hooks/useFetch";
 
 function App() {
+  const { loading, data, error } = useFetch("/api/recipes");
+
+  if (loading === true) {
+    return <p>Loading..</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Recipes recipes={data} />} />
+          <Route path="/:recipeId" element={<RecipeDetail recipes={data} />} />
+        </Routes>
+      </BrowserRouter>
+    </main>
   );
 }
 
